7fea7f3dbc724b7aaba86baf4f2fcdc8398f2ddf
McQueen <mcqueen_1993@hotmail.com>
2023-12-20 14:28:42 +0100

    uppdateringar

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Exempel } from "./interfaces"
+import { Exempel } from "./interfaces";
 
 /* let beerUrl: string */
 //beerUrl = "https://api.punkapi.com/v2/beers/random"
@@ -8,42 +8,42 @@ import { Exempel } from "./interfaces"
 // Funktion för att hämta från apin
 async function getBeer(beerUrl: string): Promise<Exempel[]> {
   try {
-    const response = await fetch(beerUrl)
-    console.log(response)
+    const response = await fetch(beerUrl);
+    console.log(response);
     if (response.status === 200) {
-      const data: Exempel[] = await response.json()
-      console.log(data)
-      return data
+      const data: Exempel[] = await response.json();
+      console.log(data);
+      return data;
     } else {
-      throw Error("Något gick fel, försök igen senare")
+      throw Error("Något gick fel, försök igen senare");
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
 // Funktion för att visa på ölen första sidan
 async function randomBeer() {
-  const data = await getBeer("https://api.punkapi.com/v2/beers/random")
-  console.log(data[0].name)
+  const data = await getBeer("https://api.punkapi.com/v2/beers/random");
+  console.log(data[0].name);
   //Hämtar img_url
-  const image = document.querySelector("#beer-image") as HTMLImageElement
+  const image = document.querySelector("#beer-image") as HTMLImageElement;
   //Lägger in bilden i html
-  image.src = data[0].image_url
+  image.src = data[0].image_url;
   //hämtar titel
-  const title = document.querySelector("#beer-name") as HTMLElement
-  title.innerHTML = data[0].name
+  const title = document.querySelector("#beer-name") as HTMLElement;
+  title.innerHTML = data[0].name;
 
   // Skickar beer id till beerinfo sida.
-  const infoButton = document.querySelector("#beer-info-link") as HTMLElement
+  const infoButton = document.querySelector("#beer-info-link") as HTMLElement;
   infoButton.onclick = function () {
-    const sendId: number = data[0].id
-    console.log("sendId", sendId)
+    const sendId: number = data[0].id;
+    console.log("sendId", sendId);
 
     // fixar urlen som sänds
-    const url: string = `beerinfo.html?id=${sendId}`
-    window.location.href = url
-  }
+    const url: string = `beerinfo.html?id=${sendId}`;
+    window.location.href = url;
+  };
 }
 
-randomBeer()
+randomBeer();
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,11 +1,33 @@
+interface malt {
+  forEach(arg0: (element: any) => void): unknown;
+  name: string;
+  amount: {
+    value: number;
+    unit: string;
+  };
+}
+
+interface hops {
+  forEach(arg0: (element: any) => void): unknown;
+  0: {
+    name: string;
+    amount: {
+      value: number;
+      unit: string;
+    };
+    add: string;
+    attribute: string;
+  };
+}
 export interface Exempel {
-  age: number
-  breed: string
-  chipNumber: string
-  image_url: string
-  name: string
-  owner: string
-  present: boolean
-  sex: string
-  id: number
+  image_url: string;
+  name: string;
+  id: number;
+  description: string;
+  abv: string;
+  ingredients: {
+    hops: hops;
+    malt: malt;
+    yeast: string;
+  };
 }
